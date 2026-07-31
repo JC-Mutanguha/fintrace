57aee8bc36ea84080497bb3caad4ba003718d59c
JC Mutanguha <mutanguhajeanclaude824@gmail.com>
2026-07-31 18:30:00 +0200

    feat: export UI component barrel

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button, ButtonLink } from "./Button";
+export { Card } from "./Card";
+export { ErrorBanner } from "./ErrorBanner";
+export { Input } from "./Input";
+export { ListDivider, ListLinkRow, ListRow, Section } from "./List";
+export { Modal } from "./Modal";
+export { Text } from "./Text";
